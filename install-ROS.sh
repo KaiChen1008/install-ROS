368069803985dc9421899596c7c9a2f7257f1298
KaiChen1008 <1038790@gmail.com>
2019-04-10 13:23:12 +0800

    ..

--- install-ROS.sh
+++ install-ROS.sh
@@ -10,10 +10,10 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 # apt-get update
-sudo apt-get update
+sudo apt-get update -y
 
 # install 
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt-get install ros-kinetic-desktop-full -y
 
 # init ROS
 sudo rosdep init
@@ -25,4 +25,4 @@ echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 . ~/.bashrc
 
 # install dependency
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
